834263a078a55289746363ca9aea2707cd8df45b
Shuhao Wu <shuhao@shuhaowu.com>
2014-12-19 16:14:01 -0500

    Added basic framework for keyboard shortcuts
    Undo is not exactly correct. We need to remember "actions".

--- js/src/canvas.js
+++ js/src/canvas.js
@@ -36,6 +36,10 @@ function Canvas() {
   this.$canvas.mousemove(this.on_mousemove.bind(this));
   this.$canvas.mouseup(this.on_mouseup.bind(this));
   this.$canvas.mouseleave(this.on_mouseleave.bind(this));
+  this.$canvas.on("contextmenu", function(e) {
+    e.preventDefault();
+  });
+  $(document).keydown(this.on_keydown.bind(this));
 
   this._points = [];
   this._painting = false;
@@ -66,6 +70,8 @@ Canvas.prototype.bind = function(ev, f) {
 Canvas.prototype.trigger = function(ev, event_obj) {
   var handlers = this._event_handlers[ev];
   if (handlers) {
+    event_obj = event_obj || {};
+    event_obj.canvas = this;
     for (var i=0; i<handlers.length; i++) {
       // TODO: preventDefault? Or Canvas gotta can?
       handlers[i](event_obj);
@@ -75,12 +81,18 @@ Canvas.prototype.trigger = function(ev, event_obj) {
   return true;
 };
 
+Canvas.prototype.clear = function() {
+  this.context.clearRect (0 , 0 , this.raw_canvas.width, this.raw_canvas.height);
+};
+
 // O(n) restore time may be bad
 Canvas.prototype.restore = function(points) {
   if (!points) {
     // This is probably the first resize_canvas we called
     return;
   }
+
+  this.clear();
 
   if (points.length <= 1) {
     this._points = points;
@@ -94,6 +106,7 @@ Canvas.prototype.restore = function(points) {
       this.draw_path(points[i]);
     }
   }
+  this.trigger("canvas_restored");
 };
 
 Canvas.prototype.get_current_pressure = function() {
@@ -142,9 +155,35 @@ Canvas.prototype.finish_path = function(reason, point) {
   this._painting = false;
 };
 
+Canvas.prototype.undo_path = function() {
+  // Such a hack. Undo path requires a restore. Restores are very slow,
+  // but it is more perfect.
+  for (var l=this._points.length, i=l-1; i>=0; i--) {
+    if (this._points[i].start) {
+      break;
+    }
+  }
+
+  this._points.splice(i, Number.MAX_VALUE);
+  this.restore(this._points);
+};
+
+Canvas.prototype.undo = function() {
+  // just this for now. As there are more things there will be more kinds of undos.
+  this.undo_path();
+};
+
 Canvas.prototype.on_mousedown = function(e) {
-  this._painting = true;
-  this.record_point(e, true);
+  switch(e.which) {
+    case 1: // Left click, draw stuff
+      this._painting = true;
+      this.record_point(e, true);
+    break;
+    case 2: // middle click
+    break;
+    case 3: // right click
+    break;
+  }
 };
 
 Canvas.prototype.on_mousemove = function(e) {
@@ -160,4 +199,18 @@ Canvas.prototype.on_mouseup = function(e) {
 
 Canvas.prototype.on_mouseleave = function(e) {
   this.finish_path("mouseleave", this.get_point_from_event(e));
+};
+
+Canvas.prototype.on_keydown = function(e) {
+  if (e.ctrlKey) {
+    switch (e.keyCode) {
+      case 83: // s
+        e.preventDefault();
+      break;
+      case 90: // z
+        this.undo();
+        e.preventDefault();
+      break;
+    }
+  }
 };
--- js/src/init.js
+++ js/src/init.js
@@ -9,6 +9,18 @@ Application.prototype.initialize = function() {
   this.canvas = new Canvas();
   this.document = new Doc();
   $("#initial-loading").remove();
+
+  var controls = $("#controls");
+  this.buttons = {};
+  this.buttons.new = $(".fa-file-o", controls).parent();
+  this.buttons.export = $(".fa-download", controls).parent();
+  this.buttons.undo = $(".fa-undo", controls).parent();
+  this.buttons.redo = $(".fa-redo", controls).parent();
+  this.buttons.share = $(".fa-file-o", controls).parent();
+  this.buttons.settings = $(".fa-cog", controls).parent();
+  this.buttons.share = $(".fa-share-alt", controls).parent();
+
+  this.buttons.undo.on("click", this.canvas.undo_path.bind(this.canvas));
 };
 
 
